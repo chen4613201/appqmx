585882f754a880b3e879ad8258d3ab3f7fc20ece
chen4613201 <493408294@qq.com>
2022-07-20 23:20:47 +0800

    first

---
+++ appqmx/admin20220713.py
@@ -0,0 +1,241 @@
+# Register your models here.
+import datetime
+
+from django.contrib import admin
+from django.db import connections
+
+from appqmx.models import KhxT, HtxT, YhxT, CbxT, jhxT
+from django.contrib import messages
+from django.utils.translation import ngettext
+
+
+# class CbxTInline(admin.TabularInline):
+#     model = CbxT
+#
+#
+# class HtxTInline(admin.TabularInline):
+#     # Score 必须是models.py中的模型名称,大小写必须要匹配.这个模型为子表,以便可以被父表编辑
+#     model = HtxT
+#     # 默认显示条目的数量
+#     # extra = 5
+
+
+class HtxTAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = ("user",)
+        form = super(HtxTAdmin, self).get_form(request, obj, **kwargs)
+        return form
+
+    list_display = (
+        'sth_HtID', 'sth_HtyfnamE', 'sth_HtqysJ', 'sth_HtjzsJ', 'sth_HtxH', 'sth_HtjH', 'sth_HtyJ', 'sth_HtjbfwF',
+        'sth_HtbyH', 'sth_HtbyC', 'sth_HtczhjE', 'sth_HtczcjE', 'sth_HtcblX', 'sth_HtczzqS', 'HtzlsC')
+    readonly_fields = ('sth_HtjfnamE',)
+    search_fields = ['sth_HtID', 'stc_CbaecnamE']
+    autocomplete_fields = ['sth_HtyfnamE']
+    form_layout = ['Ht_delete']
+    # 只读
+    fields = ('sth_HtID', ('sth_HtyfnamE', 'sth_HtfwdZ'), 'sth_HtpM', ('sth_HtxH', 'sth_HtjH'),
+              ('sth_HtyJ', 'sth_HtjbfwF'), ('sth_HtbyH', 'sth_HtbyC'), ('sth_HtczhjE', 'sth_HtczcjE'),
+              ('sth_HtqysJ', 'sth_HtjzsJ'), ('sth_HtcblX', 'sth_HtczzqS'), 'HtzlsC')
+    actions = ['add_cb']
+
+    def get_queryset(self, request):
+        qs = super(HtxTAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+    @admin.action(description='生成抄表')
+    def add_cb(self, request, queryset):
+        ht_list = queryset.values()
+        for ht in ht_list:
+            # print(ht)
+            kh_info = KhxT.objects.filter(stk_KhiD=ht['sth_HtyfnamE_id']).values('stk_KhaecnamE')
+            # hc_jhxT = HtxT.objects.filter(sth_HtyfnamE=kh_id).values('sth_HtczhjE', 'sth_HtczcjE', 'sth_HtbyH',
+            #                                                          'sth_HtbyC', 'sth_HtjbfwF')
+            ht_info = HtxT.objects.filter(sth_HtID=ht['sth_HtID']).values('sth_HtID', 'sth_HtyfnamE', 'user_id', 'sth_HtjH', 'sth_HtxH')
+            kh_name = list(kh_info)[0]['stk_KhaecnamE']
+            kh_user = list(ht_info)[0]['user_id']
+            ht_jH = list(ht_info)[0]['sth_HtjH']
+            ht_xH = list(ht_info)[0]['sth_HtxH']
+            cb_info = CbxT.objects.filter(stc_CbjH=ht_jH).values('stc_CbqC')
+
+            if not cb_info:
+                for i in range(1, int(ht['sth_HtczzqS']) + 1):
+                    CbxT.objects.create(stc_CbaecnamE=kh_name, stc_CbqC=i, user_id=kh_user, stc_CbxH=ht_xH,
+                                    stc_CbjH=ht_jH)
+
+    add_cb.icon = 'fas el-icon-check'
+    add_cb.type = 'primary'
+
+
+class KhxTAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = ("user",)
+        form = super(KhxTAdmin, self).get_form(request, obj, **kwargs)
+        return form
+
+    list_display = (
+        'stk_KhiD', 'stk_KhaecnamE', 'stk_KhjbnamE', 'stk_KhjbdH', 'stk_KhdZ', 'stk_KhlxnamE', 'stk_KhlxdH',
+        'stk_KhsydZ', 'stk_KhlB')
+    fields = (('stk_KhiD', 'stk_KhaecnamE'), ('stk_KhjbnamE', 'stk_KhjbdH', 'stk_KhdZ'),
+              ('stk_KhlxnamE', 'stk_KhlxdH', 'stk_KhsydZ'), 'stk_KhlB',)
+    search_fields = ('stk_KhaecnamE', 'stk_KhlB')
+
+    def get_queryset(self, request):
+        qs = super(KhxTAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+
+# class HtxTInline(admin.ModelAdmin):
+#   fields = (('sth_HtyfnamE', 'sth_HtqysJ',), 'sth_HtjzsJ',)
+
+
+# class CbxTInfo(admin.ModelAdmin):
+#     def pub_date(self):
+#
+#
+#
+#     pub_date.short_description = '发布日期'
+
+
+class CbxTAdmin(admin.ModelAdmin):
+    model = HtxT
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = ("user",)
+        form = super(CbxTAdmin, self).get_form(request, obj, **kwargs)
+        return form
+
+    list_display = (
+        'stc_CbqC', 'stc_CbaecnamE', 'stc_CbsycbsJ', 'stc_CbH', 'stc_CbC', 'stc_CbsH', 'stc_CbsC',
+        'stc_CbsjH', 'stc_CbsjC', 'stc_CbczhjE', 'stc_CbczcjE', 'stc_CbbyhJ', 'stc_CbbccbsJ', 'stc_CbxH', 'stc_CbjH')
+    search_fields = ('stc_CbjH', 'stc_CbaecnamE', 'stc_CbxH')
+    list_editable = ['stc_CbC', 'stc_CbH']
+    actions = ['gen_cb_data', 'delete_all_data']
+    list_filter = ['stc_CbqC']
+
+    def get_queryset(self, request):
+        qs = super(CbxTAdmin, self).get_queryset(request)
+        id_list = []
+        query_result = None
+        if request.user.is_superuser:
+            with connections['default'].cursor() as cursor:
+                cursor.execute('select min(id) from CbxT group by stc_CbjH,stc_CbbccbsJ')
+                idlist = cursor.fetchall()
+                for item in idlist:
+                    id_list.append(item[0])
+                query_result = CbxT.objects.filter(id__in=id_list)
+            # with connections['default'].cursor() as cursor:
+            #     cursor.execute('select min(id) from CbxT group by stc_CbjH,stc_CbbccbsJ')
+            #     idlist = cursor.fetchall()
+            #     for item in idlist:
+            # query_result = CbxT.objects.all()
+            # for item in query_result:
+            #     # if item['stc_CbH'] != None and item['stc_CbH'] != '' and item['stc_CbC'] != '' and item['stc_CbC'] != None:
+            #     pass
+            return query_result
+        return qs.filter(user=request.user)
+
+    # fields = (('stc_CbqC', 'stc_CbaecnamE'), 'stc_CbsycbsJ', ('stc_CbsH', 'stc_CbsC'), ('stc_CbH', 'stc_CbC'),
+    #           ('stc_CbsjH', 'stc_CbsjC'), ('stc_CbczhjE', 'stc_CbczcjE'), 'stc_CbbyhJ')
+    # autocomplete_fields = ['stc_CbaecnamE']
+    # list_editable = ['stc_CbH', 'stc_CbC']
+    # search_fields = ('stc_CbaecnamE__stk_KhaecnamE',)
+
+    @admin.action(description='生成数据')
+    def gen_cb_data(self, request, queryset):
+        cb_list = queryset.values()
+        todaytime = datetime.date.today()
+        # print(cb_list)
+        for cb in cb_list:
+            cb_previous_id = int(cb['stc_CbqC']) - 1
+            if cb_previous_id >= 0:
+                previous_cb_info = CbxT.objects.filter(stc_CbqC=cb_previous_id).values('stc_CbH', 'stc_CbC', 'stc_CbsH',
+                                                                                       'stc_CbsC', 'stc_CbbccbsJ')
+                kh_id_res = KhxT.objects.filter(stk_KhaecnamE=cb['stc_CbaecnamE']).values('stk_KhiD')
+                kh_id = list(kh_id_res)[0]['stk_KhiD']
+                hc_info = HtxT.objects.filter(sth_HtyfnamE=kh_id).values('sth_HtczhjE', 'sth_HtczcjE', 'sth_HtbyH',
+                                                                         'sth_HtbyC', 'sth_HtjbfwF')
+
+                hc_jg = list(hc_info)[0]['sth_HtczhjE']
+                cc_jg = list(hc_info)[0]['sth_HtczcjE']
+                by_h = list(hc_info)[0]['sth_HtbyH']
+                by_c = list(hc_info)[0]['sth_HtbyC']
+                ht_jbf = list(hc_info)[0]['sth_HtjbfwF']
+
+                if cb_previous_id == 0:
+                    previous_h_num = 0
+                else:
+                    previous_h_num = list(previous_cb_info)[0]['stc_CbH']
+                if cb_previous_id == 0:
+                    previous_c_num = 0
+                else:
+                    previous_c_num = list(previous_cb_info)[0]['stc_CbC']
+                previous_c_sj = todaytime
+                current_h_num = cb['stc_CbH']
+                current_c_num = cb['stc_CbC']
+                sj_h = int(current_h_num) - int(previous_h_num)
+                sj_c = int(current_c_num) - int(previous_c_num)
+
+                if sj_h > by_h:
+                    diff_num = sj_h - by_h
+                    hc_je = diff_num * hc_jg
+                else:
+                    hc_je = 0
+                if sj_c > by_c:
+                    diff_num = sj_c - by_c
+                    cc_je = diff_num * cc_jg
+                else:
+                    cc_je = 0
+                told_je = ht_jbf + cc_je + hc_je
+                queryset.update(stc_CbsjH=sj_h, stc_CbsjC=sj_c, stc_CbsH=previous_h_num,
+                                stc_CbsC=previous_c_num, stc_CbczhjE=hc_je,
+                                stc_CbczcjE=cc_je, stc_CbbyhJ=told_je, stc_CbbccbsJ=previous_c_sj, user='1')
+
+    @admin.action(description='删除所有数据')
+    def delete_all_data(self, request, queryset):
+        # qs = super(CbxTAdmin, self).get_queryset(request)
+        all_data = CbxT.objects.all()
+        all_data.delete()
+        # all_data.commit()
+
+    # delete_all_data.icon = 'fas el-icon-check'
+    delete_all_data.type = 'primary'
+    gen_cb_data.icon = 'fas el-icon-check'
+    gen_cb_data.type = 'primary'
+
+
+# class CbxtfBAdmin(admin.ModelAdmin):
+# list_display = ('dis_level',)
+# form_layout = ('sth_ HtjzsJ', 'sth_HtqysJ', 'sth_HtczzqS', 'HtzlsC', 'Ht_delete')
+
+
+admin.site.register(KhxT, KhxTAdmin)  # 将表在admin中注册
+admin.site.register(HtxT, HtxTAdmin)
+admin.site.register(YhxT)
+admin.site.register(CbxT, CbxTAdmin)
+admin.site.register(jhxT)
+# admin.site.register(user)
+admin.site.site_header = '云启智创ERP测试版'  # 设置header
+admin.site.site_title = '云启智创ERP'  # 设置title
+admin.site.index_title = '云启智创ERP'
+SIMPLEUI_HOME_TITLE = '云启智创ERP'
+
+# SIMPLEUI_DEFAULT_ICON = False
+# 服务器信息 隐藏
+SIMPLEUI_HOME_INFO = False
---
+++ appqmx/models.py
@@ -0,0 +1,214 @@
+from django.db import models
+
+# Create your models here.
+import datetime
+
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+from django.views.decorators.http import condition
+
+import appqmx.models
+
+
+class YhxT(models.Model):
+    """用户信息"""
+    SEX_CHOICE = (
+        ('0', '男'),
+        ('1', '女'),
+    )
+    stu_UxtimE = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    stu_UxnuM = models.BigIntegerField(primary_key=True, verbose_name="账号")
+    user = models.ForeignKey(User, verbose_name='用户', on_delete=models.CASCADE)
+    stu_UxpwD = models.CharField(max_length=32, verbose_name="密码")
+    stu_UxnamE = models.CharField(max_length=32, verbose_name="姓名")
+    stu_UxmZ = models.CharField(max_length=32, verbose_name="民族")
+    stu_UxxB = models.CharField(max_length=10, choices=SEX_CHOICE, verbose_name='性别', default='男')
+    stu_Uxphone = models.CharField(max_length=11, unique=True, verbose_name="手机号码")
+    stu_UxzY = models.CharField(max_length=32, verbose_name="专业", blank=True)
+    stu_UxjG = models.CharField(max_length=32, verbose_name="籍贯", blank=True)
+    stu_UxsR = models.DateField(max_length=32, verbose_name="生日", blank=True)
+
+    # 就职日期    #  是否在职  # 离职日期
+    class Meta:
+        # 末尾不加s
+        db_table = 'YhxT'
+        verbose_name = '用户信息表'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.stu_UxnamE
+
+
+def khgetohtid():
+    last_khid = KhxT.objects.all().order_by('stk_KhiD').last()
+    if not last_khid:
+        return 'YQ' + '0000001'
+    KhiD_no = last_khid.stk_KhiD
+    KhiD_int = int(KhiD_no.split('YQ')[-1])
+    new_KhiD_int = KhiD_int + 1
+    new_invoice_no = 'YQ000000' + str(new_KhiD_int)
+    return new_invoice_no
+
+
+class KhxT(models.Model):
+    """客户信息"""
+
+    LB_CHOICE = (
+        ('0', 'A'), ('1', 'B'), ('2', 'C'), ('3', 'D'), ('4', 'E'), ('5', 'F'), ('6', 'G'))
+    stk_KhiD = models.CharField(primary_key=True, max_length=11, default=khgetohtid, verbose_name="客户编号")
+    user = models.ForeignKey(User, verbose_name='用户', on_delete=models.CASCADE)
+    stk_KhaecnamE = models.CharField(max_length=25, verbose_name="企业名称")
+    stk_KhjbnamE = models.CharField(max_length=32, verbose_name="经办人")
+    stk_KhjbdH = models.CharField(max_length=32, verbose_name="电话联系方式")
+    stk_KhdZ = models.CharField(max_length=32, verbose_name="企业地址")
+    stk_KhlxnamE = models.CharField(max_length=32, verbose_name="联系人", blank=True)
+    stk_KhlxdH = models.CharField(max_length=32, verbose_name="电话联系方式", blank=True)
+    stk_KhsydZ = models.CharField(max_length=32, verbose_name="设备使用地址", blank=True)
+    stk_KhlB = models.CharField(max_length=32, choices=LB_CHOICE, verbose_name="客户类别")
+    # stk_KhlB = models.CharField(max_length=32, choices=LB_CHOICE, verbose_name="客户类别")
+
+    class Meta:
+        # 末尾不加s
+        db_table = 'KhxT'
+        verbose_name = '客户信息表'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.stk_KhaecnamE
+
+
+class KhlB(models.Model):
+    """客户类别表"""
+    stk_KbiD = models.BigIntegerField(primary_key=True, verbose_name="类别")
+    stk_KbxQ = models.CharField(max_length=32, verbose_name="详情")
+
+    class Meta:
+        # 末尾不加s
+        db_table = 'KhlB'
+        verbose_name = '客户类别表"'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.stk_KbiD
+
+
+class jhxT(models.Model):
+    """机号别表"""
+    user = models.ForeignKey(User, verbose_name='用户', on_delete=models.CASCADE)
+    jhb_pM = models.CharField(max_length=32, verbose_name="品名")
+    jhb_xH = models.CharField(max_length=32, verbose_name="型号")
+    jhb_JxiD = models.CharField(max_length=32, verbose_name="机号")
+    jhb_delete = models.BooleanField(default=False)
+
+    class Meta:
+        # 末尾不加s
+        db_table = 'jhxT'
+        verbose_name = '机号别表'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '%s%s' % (self.jhb_pM, self.jhb_JxiD)
+
+
+# def getohtid():
+#     todaytime = datetime.datetime.now()
+#     # offset = datetime.timedelta(hours=8)  # 服务器时间少8小时
+#     re_date = todaytime.strftime('%Y%m%d')
+#     num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+#     res = "HT" + re_date + "-" +
+#     return res
+
+
+class JbxT(models.Model):
+    """基本信息"""
+    stj_JbkhM = models.CharField(max_length=32, default='福建省云启智创科技有限公司', verbose_name="开户名")
+    stj_JbkhH = models.CharField(max_length=32, verbose_name="开户行")
+    stj_JbyhzH = models.CharField(max_length=32, verbose_name="账号")
+    stj_JbjbnamE = models.CharField(max_length=32, verbose_name="经办人")
+    stj_JbjbPN = models.CharField(max_length=11, verbose_name="联系电话")
+    stj_JbbZ = models.CharField(max_length=255, verbose_name="备注")
+
+    class Meta:
+        # 末尾不加s
+        db_table = 'JbxT'
+        verbose_name = '基本信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.stj_JbkhM
+
+
+def htgetohtid():
+    last_invoice = HtxT.objects.all().order_by('sth_HtID').last()
+    todaytime = datetime.datetime.now()
+    if not last_invoice:
+        return "HT" + todaytime.strftime('%Y%m%d') + '00001'
+    invoice_no = last_invoice.sth_HtID
+    invoice_int = int(invoice_no.split('HT')[-1])
+    new_invoice_int = invoice_int + 1
+    new_invoice_no = 'HT' + str(new_invoice_int)
+    return new_invoice_no
+
+
+class HtxT(models.Model):
+    SEX_CHOICE = (
+        ('0', '月'),
+        ('1', '季'),
+    )
+    sth_HtID = models.CharField(primary_key=True, max_length=32, default=htgetohtid, verbose_name="合同编号")
+    sth_HtqssJ = models.DateField(auto_now_add=True, verbose_name="合同记录起始时间")
+    user = models.ForeignKey(User, verbose_name='用户', on_delete=models.CASCADE)
+    sth_HtyfnamE = models.ForeignKey(KhxT, on_delete=models.CASCADE, verbose_name="客户名称")
+    sth_HtjfnamE = models.CharField(max_length=32, default='福建省云启智创科技有限公司', verbose_name="甲方名称")
+    sth_HtpM = models.CharField(max_length=20, verbose_name="品名(主机/周边)")
+    sth_HtxH = models.CharField(max_length=10, verbose_name="型号")
+    sth_HtjH = models.CharField(max_length=10, verbose_name="机号")
+    sth_HtfwdZ = models.CharField(max_length=32, verbose_name="服务地点")
+    sth_HtyJ = models.DecimalField(max_digits=7, decimal_places=0, verbose_name="押金金额")
+    sth_HtjbfwF = models.DecimalField(max_digits=7, decimal_places=0, verbose_name="基本费")
+    sth_HtbyC = models.DecimalField(max_digits=7, decimal_places=0, verbose_name="包印彩")
+    sth_HtbyH = models.DecimalField(max_digits=7, decimal_places=0, verbose_name="包印黑")
+    sth_HtczhjE = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="超张黑金")
+    sth_HtczcjE = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="超张彩金")
+    sth_HtcblX = models.CharField(max_length=10, choices=SEX_CHOICE, default='月', verbose_name="抄表类型")
+    sth_HtqysJ = models.DateField(verbose_name="合同生效时间")
+    sth_HtjzsJ = models.DateField(verbose_name="合同到期时间")
+    sth_HtczzqS = models.CharField(max_length=32, verbose_name="总期数", blank=True, default=12)
+    HtzlsC = models.FileField(upload_to='static/images/yyzz/', verbose_name="合同资料上传", blank=True)
+    Ht_delete = models.BooleanField(default=False, verbose_name="可抄表")
+
+    class Meta:
+        # 末尾不加s
+        db_table = 'HtxT'
+        verbose_name = '合同信息表'
+        verbose_name_plural = verbose_name
+
+
+class CbxT(models.Model):
+    """抄表系统"""
+    stc_CbqC = models.CharField(max_length=10, verbose_name="抄表期次")
+    stc_CbaecnamE = models.CharField(max_length=32, verbose_name="抄表客户信息")
+    stc_CbsycbsJ = models.CharField(max_length=32, verbose_name="上月抄表时间", blank=True)
+    user = models.ForeignKey(User, verbose_name='用户', on_delete=models.CASCADE)
+    stc_CbbccbsJ = models.CharField(max_length=32, verbose_name="本月抄表时间")
+    stc_CbH = models.DecimalField(max_digits=10, decimal_places=0, verbose_name="本月黑", blank=True, null=True)
+    stc_CbC = models.DecimalField(max_digits=10, decimal_places=0, verbose_name="本月彩", blank=True, null=True)
+    stc_CbsH = models.DecimalField(max_digits=10, decimal_places=0, verbose_name="上月黑", blank=True, null=True)
+    stc_CbsC = models.DecimalField(max_digits=10, decimal_places=0, verbose_name="上月彩", blank=True, null=True)
+    stc_CbsjH = models.DecimalField(max_digits=10, decimal_places=0, verbose_name="实际黑", blank=True, null=True)
+    stc_CbsjC = models.DecimalField(max_digits=10, decimal_places=0, verbose_name="实际彩", blank=True, null=True)
+    stc_CbczhjE = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="超张黑金额", blank=True, null=True)
+    stc_CbczcjE = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="超张彩金额", blank=True, null=True)
+    stc_CbbyhJ = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="本机合计", blank=True, null=True)
+    stc_CbxH = models.CharField(max_length=32, verbose_name="型号", null=True)
+    stc_CbjH = models.CharField(max_length=32, verbose_name="机号", null=True)
+
+    class Meta:
+        db_table = 'CbxT'
+        verbose_name = '抄表系统'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.stc_CbqC
